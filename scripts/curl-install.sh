5148543856c7cd4cbcd7855ff1ba97045a085b49
Compourri <george@compourri.co.za>
2023-11-01 15:58:39 +0200

    Create curl-install.sh

---
+++ scripts/curl-install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Checking if is running in Repo Folder
+if [[ "$(basename "$(pwd)" | tr '[:upper:]' '[:lower:]')" =~ ^scripts$ ]]; then
+    echo "You are running this in Archie Folder."
+    echo "Please use ./archie.sh instead"
+    exit
+fi
+
+# Installing git
+
+echo "Installing git."
+pacman -Sy --noconfirm --needed git glibc
+
+echo "Cloning the Archie Project"
+git clone https://github.com/Compourri/Archie
+
+echo "Executing Archie Script"
+
+cd $HOME/Archie
+
+exec ./Archie.sh
